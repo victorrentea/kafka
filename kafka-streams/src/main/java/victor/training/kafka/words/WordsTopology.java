dae7cbf35f4e2cdb1da64c71e917e96f5b604a58
Victor Rentea <victorrentea@gmail.com>
2025-02-06 20:38:52 +0200

    kafka streams simple

--- kafka-streams/src/main/java/victor/training/kafka/words/WordsTopology.java
+++ kafka-streams/src/main/java/victor/training/kafka/words/WordsTopology.java
@@ -3,6 +3,8 @@ package victor.training.kafka.words;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import victor.training.kafka.KafkaUtils;
 
@@ -14,6 +16,7 @@ public class WordsTopology {
     Properties properties = new Properties();
     properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//    properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "0"); // disable caching for faster outcome
     properties.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, "0"); // disable caching for faster outcome
     properties.put("internal.leave.group.on.close", "true"); // faster restart as per https://dzone.com/articles/kafka-streams-tips-on-how-to-decrease-rebalancing
 
@@ -31,10 +34,16 @@ public class WordsTopology {
     builder.stream("word-input", Consumed.with(Serdes.String(), Serdes.String())) // citesc
         .mapValues(v -> v.toLowerCase())
 //        .map((k,v) -> KeyValue.pair(k,v.toLowerCase())) // evita
-        .filter((k, v) -> !"hello".equals(v))
+        //.filter((k, v) -> !"hello".equals(v))
         .flatMapValues(v -> List.of(v.split("\\s+")))
         .peek((k, v) -> System.out.println("record: " + k + "-" + v))
         .to("out-topic", Produced.with(Serdes.String(), Serdes.String())); // scriu
+
+    builder.stream("out-topic", Consumed.with(Serdes.String(), Serdes.String()))
+        .groupBy((k,v)->v, Grouped.with(Serdes.String(), Serdes.String())) // repartitionare
+        .count() // de cate ori apare key KTable=tabela persistata in Kafka
+        .toStream() // emite doar la update + bufferizat
+        .to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
 
     return builder.build();
   }
