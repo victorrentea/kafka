6d154b7c1e0f195bcb488e537fae902edab23bf5
Victor Rentea <victorrentea@gmail.com>
2025-02-06 09:19:49 +0200

    polish

--- kafka-intro/src/main/java/victor/training/kafka/Producer.java
+++ kafka-intro/src/main/java/victor/training/kafka/Producer.java
@@ -8,21 +8,25 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import victor.training.kafka.Event.EventTakingLong;
-
-import java.util.concurrent.ExecutionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Component
+@RestController
 @RequiredArgsConstructor
 @Profile("!test")
 public class Producer {
   private final KafkaTemplate<String, Event> kafkaTemplate;
 
   @EventListener(ApplicationStartedEvent.class)
-  public void onAppStarted() throws Exception {
-    MDC.put("traceId", "123"); // normally setup by (a) an HTTP filter or (b) a Kafka Listener interceptor
+  public void onStartup() {
+    produceEvent();
     log.info("⭐️⭐️⭐ APP STARTED ⭐️⭐️⭐️");
+  }
+
+  @GetMapping("produce")
+  public void produceEvent() {
+    MDC.put("traceId", "123"); // pretend setup by (a) an HTTP filter or (b) a Kafka Listener interceptor
     kafkaTemplate.send("myTopic", "1", new Event.EventOK("M1"));
     // TODO send sync/async/fire-and-forget
     // TODO extract offset of sent message
