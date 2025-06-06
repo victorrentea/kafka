e5b706b6d49e2f1c9d4a1d990dbe0cdc54decd59
Victor Rentea <victorrentea@gmail.com>
2025-06-06 11:50:43 +0300

    undo

--- kafka-intro/src/main/java/victor/training/kafka/inbox/InboxPoller.java
+++ kafka-intro/src/main/java/victor/training/kafka/inbox/InboxPoller.java
@@ -23,25 +23,16 @@ public class InboxPoller {
 //   @SchedulerLock //TODO to avoid racing pods: https://www.baeldung.com/shedlock-spring
   @Scheduled(fixedRate = 500)
   public void processFromInbox() {
-    Optional<Inbox> taskOptional = inboxRepo.findNext(
-        LocalDateTime.now().minus(Duration.ofMillis(inboxTimeWindowMillis)));
-
-    if (taskOptional.isEmpty()) return;
-
-    var task = taskOptional.get();
-    log.trace("Start task {}",task);
+//    Optional<Inbox> taskOptional = inboxRepo.findNext(
+//        LocalDateTime.now().minus(Duration.ofMillis(inboxTimeWindowMillis)));
+//
+//    if (taskOptional.isEmpty()) return;
+//
+//    var task = taskOptional.get();
+//    log.trace("Start task {}",task);
 
     // TODO UNDO
-    inboxRepo.save(task.start());
-    // from here move to a background thread
-    try {
-      inboxWorker.process(task.getWork());
 
-      inboxRepo.save(task.done());
-    } catch (Exception e) {
-      log.error("Task failed: {}", task, e);
-      inboxRepo.save(task.error(e.getMessage()));
-    }
   }
 
 
--- kafka-intro/src/main/java/victor/training/kafka/inbox/InboxRepo.java
+++ kafka-intro/src/main/java/victor/training/kafka/inbox/InboxRepo.java
@@ -8,13 +8,13 @@ import java.util.Optional;
 
 public interface InboxRepo extends JpaRepository<Inbox, Long> {
   // TODO DELETE
-  @Query("""
-      select inbox
-      from Inbox inbox
-      where inbox.status = 'PENDING'
-        and inbox.receivedAt < :createdUntil
-      order by inbox.messageTimestamp
-      limit 1
-      """)
-  Optional<Inbox> findNext(LocalDateTime createdUntil);
+//  @Query("""
+//      select inbox
+//      from Inbox inbox
+//      where inbox.status = 'PENDING'
+//        and inbox.receivedAt < :createdUntil
+//      order by inbox.messageTimestamp
+//      limit 1
+//      """)
+//  Optional<Inbox> findNext(LocalDateTime createdUntil);
 }
