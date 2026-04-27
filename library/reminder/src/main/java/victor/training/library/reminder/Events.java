011d5059a3c3a9eff189891fc7c54c99f734aa08
Victor Rentea <victorrentea@gmail.com>
2026-04-27 23:28:49 +0300

    feat: add Reminder stub with Kafka consumer scaffold

---
+++ library/reminder/src/main/java/victor/training/library/reminder/Events.java
@@ -0,0 +1,15 @@
+package victor.training.library.reminder;
+
+import java.util.List;
+
+public class Events {
+    // TODO: Connect Reminder to Kafka to schedule return reminders
+
+    record BookCheckedOut(String checkoutId, long userId, List<Long> bookIds) {}
+
+    // @KafkaListener(topics = "book-checked-out", groupId = "reminder")
+    // public void onBookCheckedOut(BookCheckedOut event) {
+    //     // Schedule a reminder for event.userId() to return event.bookIds() in 14 days
+    //     log.info("Scheduling return reminder for user {} for books {}", event.userId(), event.bookIds());
+    // }
+}
